60784c0b601118bedddaf2b110a9cc1d71d7f172
kierawilson25 <60899868+kierawilson25@users.noreply.github.com>
2026-05-31 13:46:50 -0400

    Update mailing address to Berewick Town Center Drive

--- clawpoint-site/components/Footer.tsx
+++ clawpoint-site/components/Footer.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 const contactInfo = [
   { label: 'Email', value: 'CSC_growth@clawpointsecuritycollective.com', href: 'mailto:CSC_growth@clawpointsecuritycollective.com' },
-  { label: 'Location', value: '9319 Robert D. Snyder Rd, Suite 202, Charlotte, NC 28223', href: null }
+  { label: 'Location', value: '4833 Berewick Town Center Drive, Unit E, #277, Charlotte, NC 28278', href: null }
 ]
 
 const socialLinks = [
